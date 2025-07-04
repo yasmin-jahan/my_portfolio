7d9b2b86f47ddcbc49a0578e026ffb62f3490f97
yasmin-jahan <yasminjahan8603@gmail.com>
2025-07-04 15:57:40 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -97,6 +97,7 @@ $(document).ready(function () {
   //  MatchHeight
   function MatchHeight() {
     $('.portfolio_text p').matchHeight();
+    $('.portfolio_text h3').matchHeight();
   }
   MatchHeight();
 
@@ -108,14 +109,14 @@ $(document).ready(function () {
   // Scroll behavior for hamburger menu section
   let header = $('.hamburger_menu_section');
   let lastScrollTop = 0;
-  $(window).on('scroll', function () {
-    let scrollTop = $(this).scrollTop();
-    if (scrollTop > 50) {
-      header.css('top', '0'); // Show header
-    } else {
-      header.css('top', '-80px'); // Hide header
-    }
-  });
+  // $(window).on('scroll', function () {
+  //   let scrollTop = $(this).scrollTop();
+  //   if (scrollTop > 50) {
+  //     header.css('top', '0'); // Show header
+  //   } else {
+  //     header.css('top', '-80px'); // Hide header
+  //   }
+  // });
 
   //  Hamburger menu toggle
   $(".toggle_btn").click(function () {
@@ -127,5 +128,27 @@ $(document).ready(function () {
     $(".sidebar").removeClass('mobile');
     $(".overlay").fadeOut();
   });
+
+  // scroll
+ let btn = $('.button');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
+
+
+  // function MatchHeight() {
+  //   $('.portfolio_text h3').matchHeight({});
+  // }
+  // MatchHeight(); 
 
 });
